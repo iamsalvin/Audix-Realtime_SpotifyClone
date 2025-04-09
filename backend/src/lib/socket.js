2ef306a24c0b210a5532f0d5117d0e0917623261
Salvin <105032821+iamsalvin@users.noreply.github.com>
2025-04-09 12:47:03 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,7 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
 	const io = new Server(server, {
 		cors: {
-			origin: "http://localhost:3000",
+			origin: "https://audix-frontend.onrender.com",
 			credentials: true,
 		},
 	});
